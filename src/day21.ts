6d70365fb4ac74921148a47d1ac803ea0f5feb15
huess <huess@juicyarts.de>
2023-12-26 14:45:38 +0100

    feat(day 21): pre for day 21

---
+++ src/day21.ts
@@ -0,0 +1,129 @@
+import { Equal, Expect } from "./utils/testing";
+
+type TicTacToeChip = "❌" | "⭕";
+type TicTacToeEndState = "❌ Won" | "⭕ Won" | "Draw";
+type TicTacToeState = TicTacToeChip | TicTacToeEndState;
+type TicTacToeEmptyCell = "  ";
+type TicTacToeCell = TicTacToeChip | TicTacToeEmptyCell;
+type TicTacToeYPositions = "top" | "middle" | "bottom";
+type TicTacToeXPositions = "left" | "center" | "right";
+type TicTacToePositions = `${TicTacToeYPositions}-${TicTacToeXPositions}`;
+type TicTactToeBoard = TicTacToeCell[][];
+type TicTacToeGame = {
+  board: TicTactToeBoard;
+  state: TicTacToeState;
+};
+
+type FindPosition<
+  H extends any[][],
+  XPos extends any[] = [],
+  YPos extends any[] = [],
+  N extends string = "❌"
+> = H[XPos["length"]][YPos["length"]] extends N
+  ? [XPos["length"], YPos["length"]]
+  : H[XPos["length"]]["length"] extends YPos["length"]
+  ? FindPosition<H, [...XPos, 0], []>
+  : FindPosition<H, XPos, [...YPos, 0]>;
+
+type EmptyBoard = [["  ", "  ", "  "], ["  ", "  ", "  "], ["  ", "  ", "  "]];
+type Delimiter = "-";
+
+const Positions = {
+  top: 0,
+  middle: 1,
+  bottom: 2,
+  left: 0,
+  center: 1,
+  right: 2,
+} as const;
+
+type NewGame = {
+  board: EmptyBoard;
+  state: TicTacToeChip | TicTacToeEndState;
+};
+
+type PositionstringToI<T extends TicTacToePositions> =
+  T extends `${infer Y extends TicTacToeYPositions}${Delimiter}${infer X extends TicTacToeXPositions}`
+    ? [(typeof Positions)[Y], (typeof Positions)[X]]
+    : never;
+
+type UpdateBoard<Board, Move, Chip> = Board;
+
+type TicTacToe<Game extends TicTacToeGame, Move extends TicTacToePositions> = {
+  movePosition: PositionstringToI<Move>;
+  currentSide: Game["state"] extends "❌"
+    ? "⭕"
+    : Game["state"] extends "⭕"
+    ? "❌"
+    : "❌";
+
+  board: UpdateBoard<Game["board"], PositionstringToI<Move>, Game["state"]>;
+  state: Game["state"];
+};
+
+type test_move1_actual = TicTacToe<NewGame, "top-center">;
+type test_move1_expected = {
+  board: [["  ", "❌", "  "], ["  ", "  ", "  "], ["  ", "  ", "  "]];
+  state: "⭕";
+};
+type test_move1 = Expect<Equal<test_move1_actual, test_move1_expected>>;
+
+type test_move2_actual = TicTacToe<test_move1_actual, "top-left">;
+type test_move2_expected = {
+  board: [["⭕", "❌", "  "], ["  ", "  ", "  "], ["  ", "  ", "  "]];
+  state: "❌";
+};
+type test_move2 = Expect<Equal<test_move2_actual, test_move2_expected>>;
+
+type test_move3_actual = TicTacToe<test_move2_actual, "middle-center">;
+type test_move3_expected = {
+  board: [["⭕", "❌", "  "], ["  ", "❌", "  "], ["  ", "  ", "  "]];
+  state: "⭕";
+};
+type test_move3 = Expect<Equal<test_move3_actual, test_move3_expected>>;
+
+type test_move4_actual = TicTacToe<test_move3_actual, "bottom-left">;
+type test_move4_expected = {
+  board: [["⭕", "❌", "  "], ["  ", "❌", "  "], ["⭕", "  ", "  "]];
+  state: "❌";
+};
+type test_move4 = Expect<Equal<test_move4_actual, test_move4_expected>>;
+
+type test_x_win_actual = TicTacToe<test_move4_actual, "bottom-center">;
+type test_x_win_expected = {
+  board: [["⭕", "❌", "  "], ["  ", "❌", "  "], ["⭕", "❌", "  "]];
+  state: "❌ Won";
+};
+type test_x_win = Expect<Equal<test_x_win_actual, test_x_win_expected>>;
+
+type type_move5_actual = TicTacToe<test_move4_actual, "bottom-right">;
+type type_move5_expected = {
+  board: [["⭕", "❌", "  "], ["  ", "❌", "  "], ["⭕", "  ", "❌"]];
+  state: "⭕";
+};
+type test_move5 = Expect<Equal<type_move5_actual, type_move5_expected>>;
+
+type test_o_win_actual = TicTacToe<type_move5_actual, "middle-left">;
+type test_o_win_expected = {
+  board: [["⭕", "❌", "  "], ["⭕", "❌", "  "], ["⭕", "  ", "❌"]];
+  state: "⭕ Won";
+};
+
+// invalid move don't change the board and state
+type test_invalid_actual = TicTacToe<test_move1_actual, "top-center">;
+type test_invalid_expected = {
+  board: [["  ", "❌", "  "], ["  ", "  ", "  "], ["  ", "  ", "  "]];
+  state: "⭕";
+};
+type test_invalid = Expect<Equal<test_invalid_actual, test_invalid_expected>>;
+
+type test_before_draw = {
+  board: [["⭕", "❌", "⭕"], ["⭕", "❌", "❌"], ["❌", "⭕", "  "]];
+  state: "⭕";
+};
+type test_draw_actual = TicTacToe<test_before_draw, "bottom-right">;
+type test_draw_expected = {
+  board: [["⭕", "❌", "⭕"], ["⭕", "❌", "❌"], ["❌", "⭕", "⭕"]];
+  state: "Draw";
+};
+type test_draw = Expect<Equal<test_draw_actual, test_draw_expected>>;
