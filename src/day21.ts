5f164df727ee0bb56692092c1bb05a29a19175ad
huess <yildiz.h@eppendorf.de>
2024-01-02 21:05:31 +0100

    feat(day21): add day21 spaghetti

--- src/day21.ts
+++ src/day21.ts
@@ -1,5 +1,40 @@
 import { Equal, Expect } from "./utils/testing";
 
+// Replace a column in an array given its index and return the new result
+type ReplaceCol<
+  Source extends unknown[],
+  P extends number,
+  ReplaceWith,
+  ACC extends any[] = [],
+  Target extends unknown[] = []
+> = Source extends [infer Start, ...infer Rest]
+  ? ACC["length"] extends P
+    ? ReplaceCol<Rest, P, ReplaceWith, [...ACC, 0], [...Target, ReplaceWith]>
+    : ReplaceCol<Rest, P, ReplaceWith, [...ACC, 0], [...Target, Start]>
+  : Target;
+
+// Replace a desired column in an array of rows
+type ReplaceRow<
+  Source extends unknown[][],
+  P extends number[],
+  ReplaceWith,
+  ACC extends any[] = [],
+  Target extends unknown[] = []
+> = Source extends [
+  infer Start extends unknown[],
+  ...infer Rest extends unknown[][]
+]
+  ? ACC["length"] extends P[0]
+    ? ReplaceRow<
+        Rest,
+        P,
+        ReplaceWith,
+        [...ACC, 0],
+        [...Target, ReplaceCol<Start, P[1], ReplaceWith>]
+      >
+    : ReplaceRow<Rest, P, ReplaceWith, [...ACC, 0], [...Target, Start]>
+  : Target;
+
 type TicTacToeChip = "❌" | "⭕";
 type TicTacToeEndState = "❌ Won" | "⭕ Won" | "Draw";
 type TicTacToeState = TicTacToeChip | TicTacToeEndState;
@@ -13,17 +48,6 @@ type TicTacToeGame = {
   board: TicTactToeBoard;
   state: TicTacToeState;
 };
-
-type FindPosition<
-  H extends any[][],
-  XPos extends any[] = [],
-  YPos extends any[] = [],
-  N extends string = "❌"
-> = H[XPos["length"]][YPos["length"]] extends N
-  ? [XPos["length"], YPos["length"]]
-  : H[XPos["length"]]["length"] extends YPos["length"]
-  ? FindPosition<H, [...XPos, 0], []>
-  : FindPosition<H, XPos, [...YPos, 0]>;
 
 type EmptyBoard = [["  ", "  ", "  "], ["  ", "  ", "  "], ["  ", "  ", "  "]];
 type Delimiter = "-";
@@ -42,23 +66,110 @@ type NewGame = {
   state: TicTacToeChip | TicTacToeEndState;
 };
 
+// helper to translate a string position to integer positions
 type PositionstringToI<T extends TicTacToePositions> =
   T extends `${infer Y extends TicTacToeYPositions}${Delimiter}${infer X extends TicTacToeXPositions}`
     ? [(typeof Positions)[Y], (typeof Positions)[X]]
     : never;
 
-type UpdateBoard<Board, Move, Chip> = Board;
+// helper to check if a position is occupied already
+type IsOccupied<
+  Board extends unknown[][],
+  Pos extends number[]
+> = Board[Pos[0]][Pos[1]] extends TicTacToeChip ? true : false;
 
-type TicTacToe<Game extends TicTacToeGame, Move extends TicTacToePositions> = {
-  movePosition: PositionstringToI<Move>;
-  currentSide: Game["state"] extends "❌"
+// ... 1.. .1. ..1 1.. ..1 111 ... ...
+// ... 1.. .1. ..1 .1. .1. ... 111 ...
+// ... 1.. .1. ..1 ..1 1.. ... ... 111
+// too lazy for logic, so this is rahter a static approach here
+type HorizontalWinA = [[0, 0], [0, 1], [0, 2]];
+type HorizontalWinB = [[1, 0], [1, 1], [1, 2]];
+type HorizontalWinC = [[2, 0], [2, 1], [2, 2]];
+type VerticalWinA = [[0, 0], [1, 0], [2, 0]];
+type VerticalWinB = [[0, 1], [1, 1], [2, 1]];
+type VerticalWinC = [[0, 2], [1, 2], [2, 2]];
+type DiagonalWinA = [[0, 0], [1, 1], [2, 2]];
+type DiagonalWinB = [[0, 2], [1, 1], [2, 0]];
+
+type Wins =
+  | HorizontalWinA
+  | HorizontalWinB
+  | HorizontalWinC
+  | VerticalWinA
+  | VerticalWinB
+  | VerticalWinC
+  | DiagonalWinA
+  | DiagonalWinB;
+
+// if the moves collected match any of the patterns above we know its a win
+type IsWin<P extends number[][]> = P extends Wins ? true : false;
+
+// count the moves and positions of these moves for a side
+type GetMoves<
+  B extends unknown[][],
+  CP,
+  R extends number[][] = [],
+  AccX extends number[] = [],
+  AccY extends number[] = []
+> = B extends [infer Row extends unknown[], ...infer RowR extends unknown[][]]
+  ? Row extends [infer Col, ...infer ColR]
+    ? Col extends CP
+      ? GetMoves<
+          [[...ColR], ...RowR],
+          CP,
+          [...R, [AccX["length"], AccY["length"]]],
+          AccX,
+          [...AccY, 0]
+        >
+      : GetMoves<[[...ColR], ...RowR], CP, R, AccX, [...AccY, 0]>
+    : GetMoves<RowR, CP, R, [...AccX, 0], []>
+  : R;
+
+// rather dumb approach, when all fields are occupied it's a draw, no more logic
+type IsDraw<
+  B extends unknown[][],
+  R extends number[] = [],
+  AccX extends number[] = [],
+  AccY extends number[] = [],
+  Max extends number = 9
+> = B extends [infer Row extends unknown[], ...infer RowR extends unknown[][]]
+  ? Row extends [infer Col, ...infer ColR]
+    ? Col extends TicTacToeEmptyCell
+      ? IsDraw<[[...ColR], ...RowR], R, AccX, [...AccY, 0]>
+      : IsDraw<[[...ColR], ...RowR], [...R, 0], AccX, [...AccY, 0]>
+    : IsDraw<RowR, R, [...AccX, 0], []>
+  : R["length"] extends Max
+  ? true
+  : false;
+
+type TicTacToe<
+  Game extends TicTacToeGame,
+  Move extends TicTacToePositions,
+  CP extends string = Game["state"] extends "❌"
+    ? "❌"
+    : Game["state"] extends "⭕"
+    ? "⭕"
+    : "❌",
+  OP = Game["state"] extends "❌"
     ? "⭕"
     : Game["state"] extends "⭕"
     ? "❌"
-    : "❌";
-
-  board: UpdateBoard<Game["board"], PositionstringToI<Move>, Game["state"]>;
-  state: Game["state"];
+    : "⭕"
+> = {
+  board: IsOccupied<Game["board"], PositionstringToI<Move>> extends true
+    ? Game["board"]
+    : ReplaceRow<Game["board"], PositionstringToI<Move>, CP>;
+  state: IsOccupied<Game["board"], PositionstringToI<Move>> extends true
+    ? CP
+    : IsDraw<
+        ReplaceRow<Game["board"], PositionstringToI<Move>, CP>
+      > extends true
+    ? `Draw`
+    : IsWin<
+        GetMoves<ReplaceRow<Game["board"], PositionstringToI<Move>, CP>, CP>
+      > extends true
+    ? `${CP} Won`
+    : OP;
 };
 
 type test_move1_actual = TicTacToe<NewGame, "top-center">;
@@ -80,6 +191,7 @@ type test_move3_expected = {
   board: [["⭕", "❌", "  "], ["  ", "❌", "  "], ["  ", "  ", "  "]];
   state: "⭕";
 };
+
 type test_move3 = Expect<Equal<test_move3_actual, test_move3_expected>>;
 
 type test_move4_actual = TicTacToe<test_move3_actual, "bottom-left">;
@@ -94,6 +206,7 @@ type test_x_win_expected = {
   board: [["⭕", "❌", "  "], ["  ", "❌", "  "], ["⭕", "❌", "  "]];
   state: "❌ Won";
 };
+
 type test_x_win = Expect<Equal<test_x_win_actual, test_x_win_expected>>;
 
 type type_move5_actual = TicTacToe<test_move4_actual, "bottom-right">;
@@ -126,4 +239,5 @@ type test_draw_expected = {
   board: [["⭕", "❌", "⭕"], ["⭕", "❌", "❌"], ["❌", "⭕", "⭕"]];
   state: "Draw";
 };
+
 type test_draw = Expect<Equal<test_draw_actual, test_draw_expected>>;
